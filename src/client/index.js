5035c0cc2f42a895358a616bf6419b2a4b1a7f5b
Yor Abahs <ryshaba@gmail.com>
2023-02-03 21:55:36 -0500

    Imported scss files into index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,17 @@
+// Import JS Files
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
+
+// Import CSS files
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+
 
 console.log(checkForName);
 
 alert("I EXIST")
 console.log("CHANGE!!");
+
