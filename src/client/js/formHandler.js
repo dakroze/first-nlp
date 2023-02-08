6716ce9b3bda984ad0928bd6a6c84d893baadfed
Yor Abahs <ryshaba@gmail.com>
2023-02-07 23:00:27 -0500

    "New commit"

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -6,11 +6,27 @@ function handleSubmit(event) {
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+    // fetch('http://localhost:8081/test')
+    // .then(res => res.json())
+    const postData = async (url, data) => {
+        const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        credentials: 'same-origin',
+        mode: 'cors',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data) // body data type must match "Content-Type" header        
+      });
+        try {
+          const newData = await response.json();
+          console.log(newData);
+          document.getElementById('results').innerHTML = newData;
+          return newData
+        }catch(error) {
+        console.log("error oh error", error.message);
+        }
+    }
+    postData('/pr',formText)
 }
+
 
 export { handleSubmit }
