f2e9b671df92fb3205291d14e4930d7430767e09
Yor Abahs <ryshaba@gmail.com>
2023-02-09 22:38:14 -0500

    Refactor: updated design elements

--- src/client/index.js
+++ src/client/index.js
@@ -10,12 +10,6 @@ import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 
-
-console.log(checkForText);
-
-alert("I EXIST")
-
-
 // console.log("CHANGE!!");
 
 export {
